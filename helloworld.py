1f87e57eda17812534e37c2de30c50e379e22f4a
Jahnavi <82996506+Jahnavikarnati@users.noreply.github.com>
2023-05-25 18:02:13 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -3,4 +3,6 @@ print("Hello world")
 
 print("Update: This update will reflect once Jenkins auto builds after 5 minutes.")
 
+print("Rishika")
+
 print("Update 2: Testing once more, just to be sure.")
